9e003c1dbfabb455485c75f8b7960873a3fa8f20
Sayantam Dey <sayantam@gmail.com>
2016-03-08 14:19:32 +0530

    Apt-get update added

--- cookbooks/etc-environment/recipes/default.rb
+++ cookbooks/etc-environment/recipes/default.rb
@@ -4,12 +4,16 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
+apt_update 'apt_update' do
+  action :update
+end
+
 env_key = 'JAVA_OPTS'
 env_file = '/etc/environment'
 bash "java_options" do
     code <<-EOH
       echo >> #{env_file}
-      echo '#{env_key}=-Xms1024M -Xmx2048M -Xss512M' >> #{env_file}
+      echo '#{env_key}="-Xms1024M -Xmx2048M -Xss512M"' >> #{env_file}
       EOH
     not_if { ::ENV[env_key] }
 end
